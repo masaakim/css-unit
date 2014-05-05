3fbc3a0d7e88a111b9fb777a6ef09392ec6fbe77
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-05 11:37:44 +0900

    write example

---
+++ example.js
@@ -0,0 +1,29 @@
+
+var unit = require('./');
+
+unit.rm('50px'); // '50'
+unit.is('50px'); // 'px'
+unit.stats('.site-nav > li:hover .dropdown{position:absolute;top:37px;left:0;}');
+// {
+//   num: 2,
+//   units: ['px', 'none'],
+//   rules: [
+//     {
+//       selector: .site-nav > li:hover .dropdown,
+//       declarations: [
+//         {
+//           property: 'top',
+//           value: '37px',
+//           num: '37',
+//           unit: 'px'
+//         },
+//         {
+//           property: 'left',
+//           value: '0',
+//           num: '0',
+//           unit: 'none'
+//         }
+//       ]
+//     }
+//   ]
+// }
